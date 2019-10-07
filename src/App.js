73f346cd3efb2dca02800e9d5bae95489d63ad14
unknown <fryderyk.klysz@gmail.com>
2019-10-07 23:45:51 +0200

    Basic structure of project

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <p className={styles.text}>It's working!</p>
+  );
+}
+
+export default App;
